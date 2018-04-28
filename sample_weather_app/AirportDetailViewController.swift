8cca6199e72137a3e2dd42d6d715e94e36fc8327
Konrad Winkowski <winkowskikonrad@gmail.com>
2018-04-28 19:42:09 -0400

    Base for info view
    base setup for info view

---
+++ sample_weather_app/AirportDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AirportDetailViewController.swift
+//  sample_weather_app
+//
+//  Created by Konrad Winkowski on 4/28/18.
+//  Copyright © 2018 Konrad Winkowski. All rights reserved.
+//
+
+import UIKit
+
+class AirportDetailViewController: UIViewController {
+
+    @IBOutlet weak var infoSegmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func didChangeInfoType(_ sender: UISegmentedControl) {
+        
+    }
+
+}
